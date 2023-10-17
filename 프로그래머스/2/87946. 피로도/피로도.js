39bd2b16ee88cd0917b8567c05671566b3186f11
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2023-10-17 09:50:46 +0900

    [level 2] Title: 피로도, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.js
+++ 프로그래머스/2/87946. 피로도/피로도.js
@@ -1,18 +1,25 @@
 function solution(k, dungeons) {
-    let answer = 0
-    let visited = new Array(dungeons.length).fill(false)
-    
-    const DFS = (hp , count) => {
-        for(let i = 0 ; i < dungeons.length ; i++) {
-            if(!visited[i] && hp >= dungeons[i][0]){
-                visited[i] = true
-                DFS(hp - dungeons[i][1] , count + 1)
-                visited[i] = false
-            }    
-        }
-        answer = Math.max(answer, count)
+   let hp = k;
+    let depth =0;
+    let visited = new Array(dungeons.length).fill(false);
+    let result=0;
+    function DFS(count,hp)
+    {
+        for (let i=0;i<dungeons.length;i++)
+            {
+                if(!visited[i] &&hp>= dungeons[i][0])
+                    {
+                        visited[i] =true;
+                     DFS(count+1,hp-dungeons[i][1])
+                     visited[i]=false;
+                    }
+                 result = Math.max(count,result);
+            }
     }
     
-    DFS(k,0)
-    return answer
+    
+    
+    DFS(0,hp)
+    return result;
+   
 }
