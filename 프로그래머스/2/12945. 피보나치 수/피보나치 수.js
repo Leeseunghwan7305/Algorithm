2d65263a8b9be5ac5861f5d76018caf09f067fb3
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2023-10-27 13:30:43 +0900

    [level 2] Title: 피보나치 수, Time: 4.48 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.js
@@ -1,11 +1,12 @@
 function solution(n) {
-    let result = Array(n).fill(0);
+    
+    let result =[];
     result[0]=0;
     result[1]=1;
+    
     for(let i=2;i<=n;i++)
         {
-            result[i]=(result[i-1]+result[i-2])%1234567;
+            result[i]= (result[i-1]+result[i-2]) %1234567;
         }
-
-    return result[n]
+    return result[n];
 }
