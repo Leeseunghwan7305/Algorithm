3eb4d188804508b7bc826a32a78792fb6dab143b
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2023-12-18 16:11:22 +0900

    [level 2] Title: 예상 대진표, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
+++ 프로그래머스/2/12985. 예상 대진표/예상 대진표.js
@@ -1,31 +1,15 @@
 function solution(n,a,b)
 {
     let result=0;
-    while(1)
+    while(a!==b)
         {
             result++;
-                if(a%2===1)
-                    {
-                        a=(a+1)/2;
-                    }
-                else{
-                    a=a/2;
-                }
-                
-                if(b%2===1)
-                    {
-                        b=(b+1)/2;
-                    }
-                else{
-                    b=b/2;
-                }
+            a=Math.ceil(a/2);
+            b=Math.ceil(b/2);
             
-            if(a===b)
-                {
-                    return result;
-                }
             }
         
+    return result;
 }
 
 
