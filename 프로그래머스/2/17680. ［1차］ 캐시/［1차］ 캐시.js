7346c1d8923ac566407f088c61a96b573d3c5039
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2024-01-10 12:32:22 +0900

    [level 2] Title: [1차] 캐시, Time: 0.74 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,35 +1,27 @@
 function solution(cacheSize, cities) {
-    const cache=[];
-    let result=0;
-    if(cacheSize===0) return cities.length*5;
-    cities=cities.map((item)=>item.toUpperCase());
-    for(let i=0;i<Math.min(cities.length,cacheSize);i++)
-        {
-            if(cache.includes(cities[i]))
-                {
-                   cache.splice(cache.indexOf(cities[i]),1);
-                    cache.push(cities[i]);
-                    cacheSize+=1;
-                    result+=1;
-                }
-            else{
-            cache.push(cities[i]);
-            result+=5;
-            }
-     }
-    for(let i=cacheSize;i<cities.length;i++)
-        {
-            if(cache.includes(cities[i]))
-                {
-                    cache.splice(cache.indexOf(cities[i]),1);
-                    cache.push(cities[i]);
-                    result+=1;
-                }
-            else{
-            cache.shift();
-            cache.push(cities[i]);
-            result+=5;
-            }
+    const MISS=5, HIT =1;
+    
+    if(cacheSize ===0) return MISS * cities.length;
+    
+    let answer=0, cache=[];
+    
+    
+    cities.forEach(city=>{
+        city= city.toUpperCase();
+        
+        let idx= cache.indexOf(city);
+        
+        if(idx> -1){
+            cache.splice(idx,1);
+            answer+=HIT;
         }
-    return result;
+        else{
+            if(cache.length>=cacheSize) cache.shift();
+            answer+=MISS;
+        }
+        
+        cache.push(city);
+    })
+    
+    return answer;
 }
