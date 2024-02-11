98ca4285784ddd6bb1e21403751163c0f0c20355
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2024-02-11 16:05:47 +0900

    [level 2] Title: 타겟 넘버, Time: 2.20 ms, Memory: 36.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.js
@@ -1,25 +1,36 @@
 function solution(numbers, target) {
     
-    let len = numbers.length;
     let count=0;
+    //DFS 
+    1,1
+    2,2
+    3,3
     function DFS(V,sum)
     {
-        if(V===len)
+        if(V>numbers.length)
             {
-                if(sum===target)
-                    {
-                        count++;     
-                    }
                 return;
             }
+        
+       if(V===numbers.length && sum ===target)
+           {
+               count++;
+           }
         else{
-            DFS(V+1,sum+numbers[V]);
-            DFS(V+1,sum-numbers[V]);
+            DFS(V+1,sum+numbers[V])
+            DFS(V+1,sum-numbers[V])
         }
     }
 
     DFS(0,0)
-    return count;
-
-
+return count;
 }
+
+// [이진트리]
+// DFS 깊이 우선 탐색 (stack)(재귀)
+// BFS 너비 우선 탐색 (queue)
+
+// +1 -1 = target
+//+ 1 +1+1+1+1
+// -1+1+1+1+1
+
