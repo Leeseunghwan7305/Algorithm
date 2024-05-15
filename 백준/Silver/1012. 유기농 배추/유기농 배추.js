aa75cf1d6eb8dd655a75617e0c237f7cf864fe97
hwan123 <78102507+Leeseunghwan7305@users.noreply.github.com>
2024-05-16 00:09:41 +0900

    [Silver II] Title: 유기농 배추, Time: 196 ms, Memory: 12336 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.js
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.js
@@ -1,42 +1,52 @@
-let fs = require("fs");
-const { format } = require("path");
-let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+const input = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n");
 
-let [T, ...veget] = input;
-
-for (let i = 0; i < T; i++) {
-  let graph = Array.from(Array(51), () => Array(51));
-  let num = veget.shift().split(" ").map(Number);
-  let line = veget.splice(0, num[2]);
-  //제거한 요소를 담은 배열
-  //veget은 남은게 저장됨
-  let line2 = line.map((i) => i.split(" ").map(Number));
-  for (const [a, b] of line2) {
-    graph[a][b] = 1;
+let dx = [1, 0, -1, 0];
+let dy = [0, 1, 0, -1];
+function dfs(graph, n, m, x, y) {
+  // 깊이 우선 탐색(DFS) 수행
+  // 주어진 범위를 벗어나는 경우에는 즉시 종료
+  if (x <= -1 || x >= n || y <= -1 || y >= m) {
+    return false;
   }
 
-  let answer = 0;
-  let n = graph.length;
-  let dx = [-1, 0, 1, 0];
-  let dy = [0, -1, 0, 1];
-  function DFS(x, y) {
-    graph[x][y] = 0;
-    for (let k = 0; k < 8; k++) {
-      let nx = x + dx[k];
-      let ny = y + dy[k];
-      if (nx >= 0 && nx < n && ny >= 0 && ny < n && graph[nx][ny] === 1) {
-        DFS(nx, ny);
-      }
+  //현재 노드를 아직 처리하지 않았다면
+  if (graph[x][y] === 1) {
+    //해당 노드를 방문 처리
+    graph[x][y] = -1;
+
+    for (let i = 0; i < 4; i++) {
+      dfs(graph, n, m, x + dx[i], y + dy[i]);
     }
+    return true;
   }
+  return false;
+}
+
+let testCases = Number(input[0]); //테스트 케이스의 수
+let line = 1;
+while (testCases--) {
+  let [m, n, k] = input[line].split(" ").map(Number);
+  let graph = [];
   for (let i = 0; i < n; i++) {
-    for (let j = 0; j < n; j++) {
-      if (graph[i][j] === 1) {
-        graph[i][j] = 0;
+    graph[i] = new Array(m);
+  }
+
+  for (let i = 1; i <= k; i++) {
+    let [y, x] = input[line + i].split(" ").map(Number);
+    graph[x][y] = 1;
+  }
+  let answer = 0;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < m; j++) {
+      if (dfs(graph, n, m, i, j)) {
         answer++;
-        DFS(i, j);
       }
     }
   }
+  line += k + 1;
   console.log(answer);
 }
